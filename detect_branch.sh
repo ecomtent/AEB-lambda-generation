e7c13f6a55c061feabc3008a74d737a15807a774
LukeZhang0826 <110698959+LukeZhang0826@users.noreply.github.com>
2024-07-18 18:06:28 -0400

    Update detect_branch.sh

--- detect_branch.sh
+++ detect_branch.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Initialize BRANCH variable
+BRANCH="unknown"
+
 # Check if the branch name is available via webhook head ref
 if [ -n "$CODEBUILD_WEBHOOK_HEAD_REF" ]; then
   BRANCH=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | sed 's|refs/heads/||')
 else
   # Fallback to checking CODEBUILD_SOURCE_VERSION
-  if [ -n "$CODEBUILD_SOURCE_VERSION" ]; then
+  if [[ "$CODEBUILD_SOURCE_VERSION" == *"refs/heads/"* ]]; then
     BRANCH=$(echo $CODEBUILD_SOURCE_VERSION | sed 's|refs/heads/||')
-  else
-    BRANCH="unknown"
+  elif [ -n "$CODEBUILD_SOURCE_REPO_URL" ]; then
+    BRANCH=$(echo $CODEBUILD_SOURCE_REPO_URL | awk -F'/' '{print $NF}')
   fi
 fi
 
+# Determine the STAGE based on the branch name
 if [ "$BRANCH" == "main" ]; then
   export STAGE=Prod
 elif [ "$BRANCH" == "staging" ]; then
