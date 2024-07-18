b0bc8843d7d99e5eda255cf76b806d781969b1fd
LukeZhang0826 <110698959+LukeZhang0826@users.noreply.github.com>
2024-07-18 18:09:59 -0400

    Update detect_branch.sh

--- detect_branch.sh
+++ detect_branch.sh
@@ -1,26 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
 # Initialize BRANCH variable
 BRANCH="unknown"
 
-# Check if the branch name is available via webhook head ref
-if [ -n "$CODEBUILD_WEBHOOK_HEAD_REF" ]; then
-  BRANCH=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | sed 's|refs/heads/||')
+# Check if the branch name is available via webhook trigger
+if [ -n "$CODEBUILD_WEBHOOK_TRIGGER" ]; then
+  TRIGGER_TYPE=$(echo "$CODEBUILD_WEBHOOK_TRIGGER" | cut -d'/' -f1)
+  if [ "$TRIGGER_TYPE" = "branch" ]; then
+    BRANCH=$(echo "$CODEBUILD_WEBHOOK_TRIGGER" | cut -d'/' -f2)
+  elif [ "$TRIGGER_TYPE" = "pr" ]; then
+    BRANCH="pull-request"
+  elif [ "$TRIGGER_TYPE" = "tag" ]; then
+    BRANCH="tag"
+  fi
 else
-  # Fallback to checking CODEBUILD_SOURCE_VERSION
-  if [[ "$CODEBUILD_SOURCE_VERSION" == *"refs/heads/"* ]]; then
-    BRANCH=$(echo $CODEBUILD_SOURCE_VERSION | sed 's|refs/heads/||')
+  # Fallback to checking CODEBUILD_SOURCE_VERSION for branch
+  if echo "$CODEBUILD_SOURCE_VERSION" | grep -q "refs/heads/"; then
+    BRANCH=$(echo "$CODEBUILD_SOURCE_VERSION" | sed 's|refs/heads/||')
   elif [ -n "$CODEBUILD_SOURCE_REPO_URL" ]; then
-    BRANCH=$(echo $CODEBUILD_SOURCE_REPO_URL | awk -F'/' '{print $NF}')
+    BRANCH=$(echo "$CODEBUILD_SOURCE_REPO_URL" | awk -F'/' '{print $NF}')
   fi
 fi
 
 # Determine the STAGE based on the branch name
-if [ "$BRANCH" == "main" ]; then
+if [ "$BRANCH" = "main" ]; then
   export STAGE=Prod
-elif [ "$BRANCH" == "staging" ]; then
+elif [ "$BRANCH" = "staging" ]; then
   export STAGE=Stage
-elif [[ "$BRANCH" == feature-* ]]; then
+elif echo "$BRANCH" | grep -q "^feature-"; then
   export STAGE=Feat-${BRANCH#feature-}
 else
   export STAGE=Dev
