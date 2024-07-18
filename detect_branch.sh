197f77a4d7ba4cae63f2e8f98e2291a0e2833115
LukeZhang0826 <110698959+LukeZhang0826@users.noreply.github.com>
2024-07-18 17:59:36 -0400

    asd

--- detect_branch.sh
+++ detect_branch.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ "$CODEBUILD_SOURCE_VERSION" == "refs/heads/main" ]; then
+# Check for branch name using webhook head ref
+if [ -n "$CODEBUILD_WEBHOOK_HEAD_REF" ]; then
+  BRANCH=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | sed 's|refs/heads/||')
+else
+  BRANCH="unknown"
+fi
+
+if [ "$BRANCH" == "main" ]; then
   export STAGE=Prod
-elif [ "$CODEBUILD_SOURCE_VERSION" == "refs/heads/staging" ]; then
+elif [ "$BRANCH" == "staging" ]; then
   export STAGE=Stage
-elif [ "$CODEBUILD_SOURCE_VERSION" == refs/heads/feature-* ]; then
-  export STAGE=Feat-${CODEBUILD_SOURCE_VERSION#refs/heads/feature-}
+elif [[ "$BRANCH" == feature-* ]]; then
+  export STAGE=Feat-${BRANCH#feature-}
 else
   export STAGE=Dev
 fi
 
+echo "Branch: $BRANCH"
 echo "Stage: $STAGE"
