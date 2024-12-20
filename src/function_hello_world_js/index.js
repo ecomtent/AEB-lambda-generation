8b3a11d46082a68d7269c5d2608b0af31084d7fc
Cassandra Wong <cassandrawong322@gmail.com>
2024-12-20 13:08:20 -0500

    Pipeline test run

--- src/function_hello_world_js/index.js
+++ src/function_hello_world_js/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello World! From function_hello_world_js!'),
+        body: JSON.stringify('Hello World! From function_hello_world_js main!'),
     };
     return response;
 };
