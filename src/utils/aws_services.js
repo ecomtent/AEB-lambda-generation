aaacc5724636f8fb936d879895319933aec106d9
Cassandra Wong <cassandrawong322@gmail.com>
2024-12-24 17:22:07 -0500

    Add lambda crud to update single listing

--- src/utils/aws_services.js
+++ src/utils/aws_services.js
@@ -31,16 +31,18 @@ async function putObjectToS3(key, body, extention, contentType) {
 }
 
 async function websocketNotifyClients(sellerId, listingId) {
-  const params = {
-    FunctionName: process.env.WEBSOCKET_NOTIFY_CLIENTS_ARN,
-    InvocationType: 'Event', // Asynchronous invocation
-    Payload: JSON.stringify({
-      body: JSON.stringify({
-        seller_id: sellerId, 
-        listing_id: listingId, 
-        update_type: 'UPDATE_PRODUCT'
-      })
+  const requestPayload = {
+    body: JSON.stringify({
+      seller_id: seller_id,
+      listing_id: listing_id,
+      update_type: 'UPDATE_PRODUCT'
     })
+  };
+
+  const params = {
+    FunctionName: process.env.WEBSOCKET_NOTIFY_CLIENTS_ARN,  // Lambda ARN for WebSocket notifications
+    InvocationType: 'Event',  // Asynchronous invocation
+    Payload: JSON.stringify(requestPayload)
   };
 
   try {
@@ -53,4 +55,32 @@ async function websocketNotifyClients(sellerId, listingId) {
   }
 }
 
-module.exports = { putObjectToS3, dynamoDB, websocketNotifyClients };
+async function updateListing({ seller_id, listing_id, listing_updates }) {
+  const requestPayload = {
+    body: JSON.stringify({
+      seller_id: seller_id,
+      listing_id: listing_id,
+      listing_updates: listing_updates
+    })
+  };
+
+  const params = {
+    FunctionName: process.env.LISTING_CRUD_LAMBDA_ARN, 
+    InvocationType: 'RequestResponse',  // Synchronous invocation
+    Payload: JSON.stringify(requestPayload)
+  };
+
+  try {
+    const result = await lambdaClient.send(new InvokeCommand(params));
+
+    // The result will contain the Payload, which is the Lambda's response
+    const responsePayload = JSON.parse(new TextDecoder().decode(result.Payload));
+    return responsePayload;
+  } catch (error) {
+    console.error("Error invoking update listing Lambda:", error);
+    return null;
+  }
+}
+
+
+module.exports = { putObjectToS3, dynamoDB, websocketNotifyClients, updateListing };
