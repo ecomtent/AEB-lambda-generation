9a0862aea4e7f100b26d2a559c5819d87ec93d50
Cassandra Wong <cassandrawong322@gmail.com>
2024-12-24 23:12:12 -0500

    Use chromium-min package

--- src/utils/image_utils.js
+++ src/utils/image_utils.js
@@ -1,4 +1,4 @@
-const chromium = require('@sparticuz/chromium');
+const chromium = require('@sparticuz/chromium-min');
 const puppeteer = require('puppeteer-core');   
 const { createInstance } = require('polotno-node/instance');
 
