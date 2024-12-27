369443589c2c93c23158b6c21d5d6d6dc88d798a
Cassandra Wong <cassandrawong322@gmail.com>
2024-12-27 17:37:45 -0500

    Append image set to existing listing images in db

--- src/generate_image_set/index.js
+++ src/generate_image_set/index.js
@@ -1,6 +1,8 @@
 const { putObjectToS3, websocketNotifyClients, updateListing } = require('utils/aws_services');
 const { jsonToBlob } = require('utils/image_utils');
 
+const SELLER_TABLE = process.env.SELLER_TABLE_TABLE;
+
 exports.handler = async (event, context) => {
   console.log("Incoming event:", event);
   const { seller_id, listing_id, seller_email, s3benefit, s3dimension, s3lifestyle } = event;
@@ -18,6 +20,7 @@ exports.handler = async (event, context) => {
       { url: s3dimension, type: 'dimension' },
       { url: s3lifestyle, type: 'lifestyle' },
     ];
+
     const imageBlobsAndJsons = await Promise.all(
       templates.map(async ({ url, type }) => {
         const key = `${baseKey}_${type}_design_out`;
@@ -27,13 +30,13 @@ exports.handler = async (event, context) => {
         const imageSetTemplate = await fetch(url).then(response => response.json());
         const imageData = await jsonToBlob(imageSetTemplate);
 
-        return { imageData, jsonUrl, pngUrl };
+        return { imageSetTemplate, imageBlob, jsonUrl, pngUrl };
       })
     );
 
     // Uploading both JSON and PNG files to S3    
     await Promise.all(
-      imageBlobsAndJsons.map(({ imageBlob, jsonUrl, pngUrl }) => {
+      imageBlobsAndJsons.map(({ imageSetTemplate, imageBlob, jsonUrl, pngUrl }) => {
         return Promise.all([
           putObjectToS3(jsonUrl, JSON.stringify(imageSetTemplate), "json", "application/json"),
           putObjectToS3(pngUrl, imageBlob, "jpg", "image/jpg"),
@@ -47,10 +50,21 @@ exports.handler = async (event, context) => {
       polotno_json: jsonUrl
     }));
 
+    const getListingParams = {
+      TableName: SELLER_TABLE,
+      Key: { seller_id, listing_id },
+    };
+    const { Item } = await dynamoDB.send(new GetCommand(getListingParams));
+    if (!Item) {
+      throw new Error(`Listing with seller_id ${seller_id} and listing_id ${listing_id} not found`);
+    }
+    const images = Item.listing_images || [];
     const updateResult = await updateListing({
       seller_id,
       listing_id,
-      listing_updates: { listing_images: combinedData }
+      listing_updates: {
+        listing_images: [...images, ...combinedData] // append new images to existing listing images
+      }
     });
 
     if (updateResult) {
@@ -69,5 +83,6 @@ exports.handler = async (event, context) => {
 
   } catch (err) {
     console.error("Unable to process the request:", err.message);
+    return { error: err.message };
   }
 };
